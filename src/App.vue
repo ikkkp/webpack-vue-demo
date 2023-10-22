f89d0935f3b18c3191f07f9c5033be35b0453a94
ikkkp <2496450092@qq.com>
2023-10-23 00:37:03 +0800

    modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/App.vue 	modified:   src/main.js 	modified:   webpack.base.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+
+  <el-switch v-model="value1" />
+  <el-switch v-model="value2" class="ml-2" style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949" />
+  <!-- <el-scrollbar height="400px">
+    <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+  </el-scrollbar> -->
 </template>
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -10,6 +18,13 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      value1: true,
+      value2: true
+    }
+
   }
 }
 </script>
@@ -22,5 +37,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
 }
 </style>
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { VueLoaderPlugin } = require('vue-loader');
-const webpack = require('webpack');  // 引入 webpack
+const {
+    VueLoaderPlugin
+} = require('vue-loader');
+const webpack = require('webpack'); // 引入 webpack
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const {
+    ElementPlusResolver
+} = require('unplugin-vue-components/resolvers')
+
 
 module.exports = {
     entry: {
         main: './src/main',
-        vendor:'./src/vendors.js'
+        vendor: './src/vendors.js'
     },
     output: {
-        filename: 'js/[name].[fullhash].js',  // 使用 [fullhash] 替代 [hash]，这是新版本 webpack 的写法
+        filename: 'js/[name].[fullhash].js', // 使用 [fullhash] 替代 [hash]，这是新版本 webpack 的写法
         path: path.join(__dirname, 'dist'),
         publicPath: './'
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.vue$/,
                 loader: 'vue-loader'
             },
@@ -23,30 +30,43 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
                 ]
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            name: 'images/[name].[ext]'
-                        }
-                    }
-                ]
-            }
+                test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+
         ]
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
         }),
         new VueLoaderPlugin(),
         new webpack.DefinePlugin({
-            BASE_URL: JSON.stringify('./')  // 这里定义了 BASE_URL 为根路径 '/'
-        })
+            BASE_URL: JSON.stringify('./') // 这里定义了 BASE_URL 为根路径 '/'
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
     ]
 };
