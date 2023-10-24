fd7b9c88848384b34f029e9823cec5b5a66e4662
ikkkp <2496450092@qq.com>
2023-10-24 12:53:22 +0800

    modified:   jsconfig.json 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/main.js 	modified:   src/vendors.js 	modified:   webpack.base.config.js 	modified:   webpack.dev.config.js

--- src/vendors.js
+++ src/vendors.js
@@ -1,2 +1,1 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const {
     merge
 } = require('webpack-merge');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const base = require('./webpack.base.config.js');
 
 module.exports = merge(base, {    
@@ -13,6 +14,11 @@ module.exports = merge(base, {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/' // 这里设置了 publicPath
     },
+    plugins: [
+        new CleanWebpackPlugin(), // 清理输出目录
+
+        // 可以添加其他的生产环境插件或者配置
+    ]
 
 
 });
